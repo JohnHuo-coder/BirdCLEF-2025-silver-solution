cdb75094a891fbf836ec7fe277c32a257a479fa5
Jingyu Huo <huojingyu7@gmail.com>
2025-06-24 13:28:24 +0800

    Update vad_detection.py

--- src/data/preprocess/vad_detection.py
+++ src/data/preprocess/vad_detection.py
@@ -7,16 +7,13 @@ import pickle
 torch.set_num_threads(1)
 model, (get_speech_timestamps, _, read_audio, _, _) = torch.hub.load(repo_or_dir='snakers4/silero-vad', model='silero_vad')
 
-files = sorted(glob('/kaggle/input/birdclef-2025/train_audio/*/*.ogg'))
+files = sorted(glob('../../../data/raw/birdclef-2025/train_audio/*/*.ogg'))
 voice_data = {}
 for fname in files:
     wav = read_audio(fname)
     speech_timestamps = get_speech_timestamps(wav, model, return_seconds=True, threshold=0.4) # 0.4 yields best performance
     if len(speech_timestamps):
         voice_data[fname] = speech_timestamps
-
-        with open('train_voice_summary.txt', 'a') as f:
-            f.write(f'{fname}\n')
             
-with open('train_voice_data_0.4.pkl', 'wb') as f:
+with open('../../../data/processed/train_voice_data_0.4.pkl', 'wb') as f:
     pickle.dump(voice_data, f)
