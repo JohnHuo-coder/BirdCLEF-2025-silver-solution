4dfa70b9aec636ba4e2fc6d29df3fa23a4031cba
Jingyu Huo <huojingyu7@gmail.com>
2025-06-25 17:07:38 +0800

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -1,2 +1,3 @@
 kaggle competitions download -c birdclef-2025 -p ./data/raw/
-unzip ./data/raw/birdclef-2025.zip -d ./data/raw/
+mkdir ./data/raw/birdclef-2025
+unzip ./data/raw/birdclef-2025.zip -d ./data/raw/birdclef-2025/
